2689161b0ffdcce8b7b571414a747687b2b7526c
Tatiana <tanya_potapenko@bigmir.net>
2024-08-22 11:49:20 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,28 @@
+"use strict";
+const promise1 = new Promise((resolve)=>{
+    const logo = document.querySelector(".logo");
+    logo.addEventListener("click", ()=>{
+        resolve("Promise was resolved!");
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        reject(new Error("Promise was rejected!"));
+    }, 3000);
+});
+function logoResolve(message, isError = false) {
+    const logo = document.createElement("div");
+    logo.className = "message";
+    if (logo) logo.textContent = message;
+    document.body.append(logo);
+}
+function logoRejected(message, isError = false) {
+    const logo = document.createElement("div");
+    logo.className = "message error-message";
+    if (logo) logo.textContent = message;
+    document.body.append(logo);
+}
+promise1.then((message)=>logoResolve(message)).catch((error)=>logoResolve(error.message, true));
+promise2.then((message)=>logoRejected(message)).catch((error)=>logoRejected(error.message, true));
+
+//# sourceMappingURL=index.f75de5e1.js.map
